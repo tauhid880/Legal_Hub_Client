22e1bf2b38b5a743f02e5e7c9b05daf0edc37269
tauhid880 <tauhid880@gmail.com>
2022-11-08 20:33:42 +0600

    ContactUs section added

---
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Lottie from "lottie-react";
+import Animation from "../../../assets/contact-us.json";
+
+const ContactUs = () => {
+  return (
+    <div className="grid w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto  md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 bg-gray-600 text-gray-100">
+      <div className="flex flex-col justify-between">
+        <div className="space-y-2">
+          <h2 className="text-4xl font-bold leading-tight lg:text-5xl">
+            Let's talk!
+          </h2>
+          <div className="text-gray-400">Vivamus in nisl metus? Phasellus.</div>
+        </div>
+        <Lottie animationData={Animation} loop={true}></Lottie>
+      </div>
+      <form novalidate="" className="space-y-6 ">
+        <div>
+          <label for="name" className="text-sm">
+            Full name
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder=""
+            className="w-full p-3 rounded mt-2"
+          />
+        </div>
+        <div>
+          <label for="email" className="text-sm">
+            Email
+          </label>
+          <input id="email" type="email" className="w-full p-3 rounded mt-2" />
+        </div>
+        <div>
+          <label for="message" className="text-sm">
+            Message
+          </label>
+          <textarea
+            id="message"
+            rows="3"
+            className="w-full p-3 rounded mt-2"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded bg-violet-400 text-gray-900"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ContactUs from "./ContactUs/ContactUs";
 import Hero from "./Hero/Hero";
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
+      <ContactUs></ContactUs>
     </div>
   );
 };
